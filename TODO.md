3ab93ca9b2248778238ccd38547ba0cfe416ff43
wbio <bogeywm@gmail.com>
2016-03-06 23:59:54 -0500

    Moved flow to Jade

--- TODO.md
+++ TODO.md
@@ -19,7 +19,7 @@
 
 
 ### Refactoring: ###
-- Move flow.html to a Jade template
+- ~~Move flow.html to a Jade template~~
 - Move logic out of routes on server side
 - Look into floweditor.js and main.js client libraries
 - Move storage to Postgres? - Maybe not worth the LOE
@@ -29,9 +29,6 @@
 ## Old To Do: ##
 
 ### Quick TODO: ###
-- When a page title is changed, that should be reflected in the page list (only after 'Apply')
-  - Current page should be hidden from the list, to prevent us from running in to weird experiences where we could link to the same page or where we've changed the page name but not applied
-- When jumping to a new page, if Apply is enabled, we should have to confirm that we don't want to save
 - When animating the new page load, we should also scroll to the top
 - Pressing Enter on the Page Name should blur() the box (and not result in a new line)
   - Whenever the blur() event is fired, we should check to see if the text has changed, and if so then enable 'Apply' (but not change the text in the Pages until it is applied)
--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -10,7 +9,7 @@ var multer  = require('multer');
 var mongo = require('mongoskin');
 var db = mongo.db("mongodb://localhost:27017/mobilepathing", {native_parser:true});
 
-var pages = require('./routes/flow');
+var flow = require('./routes/flow');
 
 var app = express();
 
@@ -19,7 +18,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,44 +28,46 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
-  req.db = db;
-  next();
+	req.db = db;
+	next();
 });
 
-app.use('/', routes);
-app.use('/', pages);
+// Attach the flow routes to the router
+app.use('/', flow);
+
+
+
+// error handlers
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
-
-// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: err
-  });
-  console.log(err.message);
-  console.log(err);
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: err
+	});
+	console.log(err.message);
+	console.log(err);
 });
 
 
